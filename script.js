913f9109267d25fecd2de72acfd7891ef1134bed
joey bennett <joeykb17@gmail.com>
2022-11-04 11:52:31 -0700

    add comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//create variables with pointers to html elements
 let tab1 = document.getElementById('tab1');
 let tab2 = document.getElementById('tab2');
 let tab3 = document.getElementById('tab3');
@@ -10,9 +11,13 @@ let paragraph1 = document.getElementById('paragraph1');
 let paragraph2 = document.getElementById('paragraph2');
 let paragraph3 = document.getElementById('paragraph3');
 
+//add click event listener to the first tab
 tab1.addEventListener('click', (event) => {
+    //update the current tab to have selected styling and display paragraph1
     tab1.style = "background:black;color:white;margin:auto; width:33%; padding:6px;";
     paragraph1.style="display:inline;padding:16px;";
+
+    //update other tabs to be hidden
     tab2.style = "margin:auto; width:33%; padding:6px; border-left:solid;"
     paragraph2.style="display:none";
     tab3.style = "margin:auto; width:33%; padding:6px;border-left:solid;"
@@ -20,8 +25,11 @@ tab1.addEventListener('click', (event) => {
 });
 
 tab2.addEventListener('click', (event) => {
+    //update the current tab to have selected styling and display paragraph2
     tab2.style = "background:black;color:white;margin:auto; width:33%; padding:6px;";
     paragraph2.style="display:inline;padding:16px;";
+
+    //update other tabs to be hidden
     tab1.style = "background:white;color:black;margin:auto; width:33%; padding:6px;"
     paragraph1.style="display:none";
     tab3.style = "background:white;color:black;margin:auto; width:33%; padding:6px;border-left:solid;"
@@ -29,8 +37,11 @@ tab2.addEventListener('click', (event) => {
 });
 
 tab3.addEventListener('click', (event) => {
+    //update the current tab to have selected styling and display paragraph3
     tab3.style = "background:black;color:white;margin:auto; width:33%; padding:6px;";
     paragraph3.style="display:inline;padding:16px;";
+
+    //update other tabs to be hidden
     tab1.style = "background:white;color:black;margin:auto; width:33%; padding:6px;"
     paragraph1.style="display:none";
     tab2.style = "background:white;color:black;margin:auto; width:33%; padding:6px;border-left:solid;"
